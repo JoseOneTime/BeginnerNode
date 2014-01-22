a6ec0587e59c9b9736070cb58d8ce0016512c917
JoseOneTime <claveren@gmail.com>
2014-01-22 00:44:28 -0500

    Make server.js a module

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var http = require('http');
 
-http.createServer(function(req, res){
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.write("Hello World");
-	res.end();
-}).listen(8000);
-console.log('listening on port 8000...');
+function start(){
+	http.createServer(function(req, res){
+		res.writeHead(200, {"Content-Type": "text/plain"});
+		res.write("Hello World");
+		res.end();
+	}).listen(8000);
+	console.log('listening on port 8000...');
+};
+
+exports.start = start;
